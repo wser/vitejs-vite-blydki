73427a598d771aa01d4740d750bb578c65a58a6e
wser <w.web.dev@gmail.com>
2023-05-19 15:02:55 +0200

    1 file modified, 3 added, 3 deleted

--- data-set-loader.js
+++ data-set-loader.js
@@ -1,56 +1,62 @@
 function getGraphDataSets() {
-
-  const loadMiserables = function(Graph) {
-      Graph
-        .nodeLabel('id')
-        .nodeAutoColorBy('group')
-        .jsonUrl('.miserables.json');
+  const loadMiserables = function (Graph) {
+    Graph.nodeLabel('id')
+      .nodeAutoColorBy('group')
+      .jsonUrl('./data/miserables.json');
   };
-  loadMiserables.description = "<em>Les Miserables</em> data (<a href='https://bl.ocks.org/mbostock/4062045'>4062045</a>)";
+  loadMiserables.description =
+    "<em>Les Miserables</em> data (<a href='https://bl.ocks.org/mbostock/4062045'>4062045</a>)";
 
   //
 
-  const loadBlocks = function(Graph) {
-      Graph
-        .nodeLabel(node => `${node.user?node.user+': ':''}${node.description || node.id}`)
-        .nodeAutoColorBy('user')
-        .jsonUrl('.blocks.json');
+  const loadBlocks = function (Graph) {
+    Graph.nodeLabel(
+      (node) =>
+        `${node.user ? node.user + ': ' : ''}${node.description || node.id}`
+    )
+      .nodeAutoColorBy('user')
+      .jsonUrl('./data/blocks.json');
   };
-  loadBlocks.description = "<em>Blocks</em> data (<a href='https://bl.ocks.org/mbostock/afecf1ce04644ad9036ca146d2084895'>afecf1ce04644ad9036ca146d2084895</a>)";
+  loadBlocks.description =
+    "<em>Blocks</em> data (<a href='https://bl.ocks.org/mbostock/afecf1ce04644ad9036ca146d2084895'>afecf1ce04644ad9036ca146d2084895</a>)";
 
   //
 
-  const loadD3Dependencies = function(Graph) {
-      fetch('.d3.csv').then(r => r.text()).then(d3.csvParse).then(data => {
-          const nodes = [], links = [];
-          data.forEach(({ size, path }) => {
-              const levels = path.split('/'),
-                module = levels.length > 1 ? levels[1] : null,
-                leaf = levels.pop(),
-                parent = levels.join('/');
+  const loadD3Dependencies = function (Graph) {
+    fetch('./data/d3_deps.csv')
+      .then((r) => r.text())
+      .then(d3.csvParse)
+      .then((data) => {
+        const nodes = [],
+          links = [];
+        data.forEach(({ size, path }) => {
+          const levels = path.split('/'),
+            module = levels.length > 1 ? levels[1] : null,
+            leaf = levels.pop(),
+            parent = levels.join('/');
 
-              nodes.push({
-                  path,
-                  leaf,
-                  module,
-                  size: +size || 1
-              });
-
-              if (parent) {
-                  links.push({ source: parent, target: path});
-              }
+          nodes.push({
+            path,
+            leaf,
+            module,
+            size: +size || 1,
           });
 
-          Graph
-              .nodeRelSize(0.5)
-              .nodeId('path')
-              .nodeVal('size')
-              .nodeLabel('path')
-              .nodeAutoColorBy('module')
-              .graphData({ nodes, links });
+          if (parent) {
+            links.push({ source: parent, target: path });
+          }
+        });
+
+        Graph.nodeRelSize(0.5)
+          .nodeId('path')
+          .nodeVal('size')
+          .nodeLabel('path')
+          .nodeAutoColorBy('module')
+          .graphData({ nodes, links });
       });
   };
-  loadD3Dependencies.description = "<em>D3 dependencies</em> data (<a href='https://bl.ocks.org/mbostock/9a8124ccde3a4e9625bc413b48f14b30'>9a8124ccde3a4e9625bc413b48f14b30</a>)";
+  loadD3Dependencies.description =
+    "<em>D3 dependencies</em> data (<a href='https://bl.ocks.org/mbostock/9a8124ccde3a4e9625bc413b48f14b30'>9a8124ccde3a4e9625bc413b48f14b30</a>)";
 
   //
 
