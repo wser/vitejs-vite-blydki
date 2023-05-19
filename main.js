20a926c052fcab0098d1bd893e5646a835711ecf
wser <w.web.dev@gmail.com>
2023-05-19 14:59:06 +0200

    2 files modified

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-const Graph = ForceGraph3D()
-	(document.getElementById("3d-graph"));
+const Graph = ForceGraph3D()(document.getElementById('3d-graph'));
 
 let curDataSetIdx,
-	numDim = 2;
+  numDim = 2; // 1,2,3
 
 const dataSets = getGraphDataSets();
 
 let toggleData;
-(toggleData = function() {
-	curDataSetIdx = curDataSetIdx === undefined ? 0 : (curDataSetIdx+1)%dataSets.length;
+(toggleData = function () {
+  curDataSetIdx =
+    curDataSetIdx === undefined ? 0 : (curDataSetIdx + 1) % dataSets.length;
 
-	const dataSet = dataSets[curDataSetIdx];
-	document.getElementById('graph-data-description').innerHTML = dataSet.description ? `Viewing ${dataSet.description}` : '';
+  const dataSet = dataSets[curDataSetIdx];
+  document.getElementById('graph-data-description').innerHTML =
+    dataSet.description ? `Viewing ${dataSet.description}` : '';
 
-	dataSet(Graph
-		.resetProps()
-		.enableNodeDrag(false)
-		.numDimensions(numDim)
-	);
+  dataSet(Graph.resetProps().enableNodeDrag(true).numDimensions(numDim));
 })(); // IIFE init
 
-const toggleDimensions = function(numDimensions) {
-	numDim = numDimensions;
-	Graph.numDimensions(numDim);
+const toggleDimensions = function (numDimensions) {
+  numDim = numDimensions;
+  Graph.numDimensions(numDim);
 };
